467d65b2a7b0e4bf445c45768132345cb406dbd1
Lesedi Seleke <selekelesedi25@gmail.com>
2025-01-10 16:34:42 +0200

    Did exceptions class, to do: database connection properties

--- EnviroAssessLesedi/src/main/java/com/enviro/assessment/grad001/lesediseleke/EnviroAssessLesedi/Assessment/ExceptionHandling.java
+++ EnviroAssessLesedi/src/main/java/com/enviro/assessment/grad001/lesediseleke/EnviroAssessLesedi/Assessment/ExceptionHandling.java
@@ -1,4 +1,23 @@
 package com.enviro.assessment.grad001.lesediseleke.EnviroAssessLesedi.Assessment;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class ExceptionHandling {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception){
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 }
